32b748195557c5542f3c7513815642f5eda3d79a
Jed <vjeudy@protonmail.com>
2024-03-19 21:55:56 +0100

    updates migration file

--- db/migrations/1_init_db.sql
+++ db/migrations/1_init_db.sql
@@ -1,13 +1,13 @@
-CREATE TABLE IF NOT EXISTS user (
+CREATE TABLE IF NOT EXISTS author (
     id INT PRIMARY KEY,
-    name VARCHAR(20) not null,
-    email VARCHAR(20) not null
+    name VARCHAR(20) NOT NULL,
+    email VARCHAR(20) NOT NULL
 );
 
 CREATE TABLE IF NOT EXISTS article (
     id INT PRIMARY KEY,
-    description VARCHAR(100) not null, 
-    content VARCHAR(500) not null,
+    description VARCHAR(100) NOT NULL, 
+    content VARCHAR(500) NOT NULL,
     user_id INT not null
 );
 
