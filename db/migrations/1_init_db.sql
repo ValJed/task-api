0aeb4d6d5b9deed72c6bf33733334ea8a064263e
Jed <vjeudy@protonmail.com>
2024-03-23 15:38:54 +0100

    updates migration to match new data

--- db/migrations/1_init_db.sql
+++ db/migrations/1_init_db.sql
@@ -1,13 +1,11 @@
-CREATE TABLE IF NOT EXISTS author (
-    id INT PRIMARY KEY,
-    name VARCHAR(20) NOT NULL,
-    email VARCHAR(20) NOT NULL
+CREATE TABLE IF NOT EXISTS task (
+    id SERIAL PRIMARY KEY,
+    content VARCHAR(1000) NOT NULL,
+    context_id INT NOT NULL
 );
 
-CREATE TABLE IF NOT EXISTS article (
-    id INT PRIMARY KEY,
-    description VARCHAR(100) NOT NULL, 
-    content VARCHAR(500) NOT NULL,
-    user_id INT not null
+CREATE TABLE IF NOT EXISTS context (
+    id SERIAL PRIMARY KEY,
+    name VARCHAR(100) NOT NULL
 );
 
