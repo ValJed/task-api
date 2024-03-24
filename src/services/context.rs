9f4f380cb9c0b535eb8d4ff3503a3d97e24a3b33
Jed <vjeudy@protonmail.com>
2024-03-24 22:17:58 +0100

    wip implements context routes

--- src/services/context.rs
+++ src/services/context.rs
@@ -1,9 +1,9 @@
 #[path = "../structs.rs"]
 mod structs;
 
-use actix_web::{delete, get, post, put, web, HttpRequest, HttpResponse, Responder, Scope};
-use sqlx;
-use structs::AppState;
+use actix_web::{delete, get, post, put, web, HttpRequest, HttpResponse, Responder, Result, Scope};
+use sqlx::{postgres::types::PgLQueryLevel, Pool, Postgres};
+use structs::{Context, ContextRequest, RequestError};
 
 pub fn get_scope() -> Scope {
     web::scope("/context")
@@ -14,22 +14,63 @@ pub fn get_scope() -> Scope {
 }
 
 #[get("")]
-pub async fn fetch_all(data: AppState) -> impl Responder {
-    println!("data: {:?}", data);
-    HttpResponse::Ok().body("fetch contexts")
+pub async fn fetch_all(pool: web::Data<Pool<Postgres>>) -> Result<impl Responder, RequestError> {
+    let contexts: Result<Vec<Context>, sqlx::Error> = sqlx::query_as("SELECT * FROM context")
+        .fetch_all(pool.get_ref())
+        .await;
+
+    if contexts.is_err() {
+        return Err(RequestError::InternalServerError);
+    }
+
+    Ok(web::Json(contexts.unwrap()))
 }
 
 #[post("")]
-pub async fn fetch_or_create(data: AppState) -> impl Responder {
-    HttpResponse::Ok().body("create context")
+pub async fn fetch_or_create(
+    pool: web::Data<Pool<Postgres>>,
+    data: web::Json<ContextRequest>,
+) -> Result<impl Responder, RequestError> {
+    if data.name.is_empty() {
+        return Err(RequestError::BadRequest);
+    };
+
+    let existing: Result<Option<Context>, sqlx::Error> =
+        sqlx::query_as("SELECT * FROM context WHERE name = $1")
+            .bind(data.name.clone())
+            .fetch_optional(pool.get_ref())
+            .await;
+
+    if existing.is_err() {
+        return Err(RequestError::InternalServerError);
+    }
+
+    let ctx = existing.unwrap();
+
+    println!("ctx: {:?}", ctx);
+    if ctx.is_some() {
+        return Ok(web::Json(ctx.unwrap()));
+    }
+
+    let context: Result<Context, sqlx::Error> =
+        sqlx::query_as("INSERT INTO context (name) VALUES ($1)")
+            .bind(data.name.clone())
+            .fetch_one(pool.get_ref())
+            .await;
+
+    if context.is_err() {
+        return Err(RequestError::InternalServerError);
+    }
+
+    Ok(web::Json(context.unwrap()))
 }
 
 #[put("/{id}")]
-pub async fn update(data: AppState) -> impl Responder {
+pub async fn update(pool: web::Data<Pool<Postgres>>, req: HttpRequest) -> impl Responder {
     HttpResponse::Ok().body("update context")
 }
 
 #[delete("/")]
-pub async fn delete(data: AppState) -> impl Responder {
+pub async fn delete(pool: web::Data<Pool<Postgres>>, req: HttpRequest) -> impl Responder {
     HttpResponse::Ok().body("delete context")
 }
