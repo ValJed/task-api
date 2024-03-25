8bc0cc5f00cf53f1b2058890b727c0cb1b1d5488
Jed <vjeudy@protonmail.com>
2024-03-25 21:55:58 +0100

    simplifies by removing RequestError struct (handle error through HttpResponse)

--- src/structs.rs
+++ src/structs.rs
@@ -1,7 +1,3 @@
-use std::fmt::Display;
-
-use actix_web::{http::header::ContentType, http::StatusCode, HttpResponse, ResponseError};
-use derive_more::{Display, Error};
 use serde::{Deserialize, Serialize};
 use sqlx::{FromRow, Pool, Postgres};
 
@@ -28,29 +24,4 @@ pub struct Task {
 #[derive(Deserialize, Debug, Clone)]
 pub struct ContextRequest {
     pub name: String,
-}
-
-#[derive(Debug, Display, Error)]
-pub enum RequestError {
-    NotFound,
-    BadRequest,
-    InternalServerError,
-    Forbidden,
-}
-
-impl ResponseError for RequestError {
-    fn error_response(&self) -> HttpResponse {
-        HttpResponse::build(self.status_code())
-            .insert_header(ContentType::html())
-            .body(self.to_string())
-    }
-
-    fn status_code(&self) -> StatusCode {
-        match *self {
-            RequestError::NotFound => StatusCode::NOT_FOUND,
-            RequestError::BadRequest => StatusCode::BAD_REQUEST,
-            RequestError::InternalServerError => StatusCode::INTERNAL_SERVER_ERROR,
-            RequestError::Forbidden => StatusCode::FORBIDDEN,
-        }
-    }
 }
