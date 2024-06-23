3781395559548d1efd790ff98ec916673a32cc95
Jed <vjeudy@protonmail.com>
2024-06-23 10:41:45 +0200

    creates db at startup

--- src/db.rs
+++ src/db.rs
@@ -1,7 +1,19 @@
-use sqlx::{postgres::PgPoolOptions, Pool, Postgres};
+use sqlx::{migrate::MigrateDatabase, postgres::PgPoolOptions, Pool, Postgres};
 
 pub async fn connect_db() -> Pool<Postgres> {
     let url = std::env::var("DATABASE_URL").expect("DATABASE_URL must be set");
+
+    let db_exists = Postgres::database_exists(&url)
+        .await
+        .expect("Error while checking if the database exists");
+
+    if !db_exists {
+        let created_res = Postgres::create_database(&url).await;
+        if created_res.is_err() {
+            panic!("Error while creating the database");
+        }
+    }
+
     let pool = PgPoolOptions::new()
         .max_connections(5)
         .connect(&url)
