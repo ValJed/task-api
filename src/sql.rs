4b1f5fcdd1f003d5c32ed44a0be331bb3d66b5e7
Jed <vjeudy@protonmail.com>
2025-06-23 23:35:01 +0200

    rewrites task sql requests to work with sqlite

--- src/sql.rs
+++ src/sql.rs
@@ -3,19 +3,20 @@ pub const LIST_TASKS_ACTIVE: &str = r#"
         context.id,
         context.name,
         context.active,
-        COALESCE(json_agg(
-            json_build_object(
-                'id', task.id, 
-                'content', task.content, 
-                'done', task.done, 
-                'creation_date', task.creation_date, 
-                'modification_date', task.modification_date) ORDER BY task.id ASC
-            ) FILTER (WHERE task.id IS NOT NULL), '[]') AS tasks
+        COALESCE(
+            json_group_array(
+                json_object(
+                    'id', task.id,
+                    'content', task.content,
+                    'done', task.done,
+                    'creation_date', task.creation_date,
+                    'modification_date', task.modification_date
+                )
+            ) FILTER (WHERE task.id IS NOT NULL), json_array()) AS tasks
         FROM context
-        LEFT JOIN task
-        ON task.context_id = context.id
-        WHERE context.active = true
-        GROUP BY context.id
+        LEFT JOIN task ON task.context_id = context.id
+        WHERE context.active = 1
+        GROUP BY context.id, context.name, context.active
         ORDER BY context.id ASC;
         "#;
 
@@ -24,17 +25,18 @@ pub const LIST_TASKS: &str = r#"
         context.id,
         context.name,
         context.active,
-        COALESCE(json_agg(
-            json_build_object(
-                'id', task.id, 
-                'content', task.content, 
-                'done', task.done, 
-                'creation_date', task.creation_date, 
-                'modification_date', task.modification_date) ORDER BY task.id ASC
-            ) FILTER (WHERE task.id IS NOT NULL), '[]') AS tasks
+        COALESCE(
+            json_group_array(
+                json_object(
+                    'id', task.id,
+                    'content', task.content,
+                    'done', task.done,
+                    'creation_date', task.creation_date,
+                    'modification_date', task.modification_date
+                )
+            ) FILTER (WHERE task.id IS NOT NULL), json_array()) AS tasks
         FROM context
-        LEFT JOIN task
-        ON task.context_id = context.id
-        GROUP BY context.id
+        LEFT JOIN task ON task.context_id = context.id
+        GROUP BY context.id, context.name, context.active
         ORDER BY context.id ASC;
         "#;
